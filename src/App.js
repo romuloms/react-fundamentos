5376791f87c792385768cc449107556fe1497b0f
romuloms <romulomsantana@hotmail.com>
2023-05-29 19:51:18 -0300

    applied props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import Post from './Post';
 
-const category = 'Posts da semana';
+// const tituloUm = 'Título UM';
 
 function App() {
   return (
     <>
       <h1>JStack's Blog</h1>
-      <h2>{category}</h2>
+      <h2>Posts da semana</h2>
+
+      <hr/>
+
+      <Post 
+        post={{
+          title: 'Título UM',
+          subtitle: 'Subtítulo da notícia 01',
+        }}
+      />
+      <Post 
+        post={{
+          title: 'Título DOIS',
+          subtitle: 'Subtítulo da notícia 02',
+        }}
+      />
     </>
   );
 }
