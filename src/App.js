35b788357d40f33f9bac184b730ece32a7dc21d0
romuloms <romulomsantana@hotmail.com>
2023-05-31 18:14:12 -0300

    applied useState on posts

--- src/App.js
+++ src/App.js
@@ -1,38 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Post from './Post';
 import Header from './Header';
 
-const posts = [
-  { 
-    title: 'Titulo#01',
-    subtitle: 'Subtitulo#01',
-    likes: 10
-  },
-  { 
-    title: 'Titulo#02',
-    subtitle: 'Subtitulo#02',
-    likes: 20
-  },
-  { 
-    title: 'Titulo#03',
-    subtitle: 'Subtitulo#03',
-    likes: 100
-  },
-];
-
 function App() {
+  const [posts, setPosts] = useState([
+    { 
+      id: Math.random(),
+      title: 'Titulo#01',
+      subtitle: 'Subtitulo#01',
+      likes: 10
+    },
+    { 
+      id: Math.random(),
+      title: 'Titulo#02',
+      subtitle: 'Subtitulo#02',
+      likes: 20
+    },
+    { 
+      id: Math.random(),
+      title: 'Titulo#03',
+      subtitle: 'Subtitulo#03',
+      likes: 100
+    },
+  ]);
+
+  function handleRefresh() {
+    setPosts((prevState) => [
+      // é boa pratica usar o argumento prevState porque
+      // evita o uso direto do array posts e evita
+      // possíveis problemas na renderizacao do codigo
+      ...prevState,
+      {
+        id: Math.random(),
+        title: `Título#0${prevState.length + 1}`,
+        subtitle: `Sub#0${prevState.length + 1}`,
+        likes: 20
+      },
+    ]);
+  }
+
   return (
     <>
       <Header>
-        <h2>Posts da semana</h2>
+        <h2>
+          Posts da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </ Header>
 
       <hr/>
 
       {posts.map(post => (
         <Post 
-          key={post.title}
+          key={post.id}
           likes={post.likes}
           post={{
             title: post.title,
