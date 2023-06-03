9d2743562fa9c1d9da862dd588283442ce1495ab
romuloms <romulomsantana@hotmail.com>
2023-06-03 18:50:01 -0300

    created index.js

--- src/Header.js
+++ src/Header.js
@@ -8,19 +8,13 @@ export default function Header(props) {
   const { onToggleTheme } = useContext(ThemeContext);
 
   return (
-    <div
-      style={{
-        background: '#ccc',
-        fontFamily: 'sans-serif',
-        marginBottom: '2rem',
-      }}
-    >
+    <>
       <h1>{props.title}</h1>
       <Button onClick={onToggleTheme}>
         Mudar tema
       </Button>
       {props.children}
-    </div>
+    </>
   );
 }
 
