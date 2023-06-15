a43e0c605975ed636f3749aca5c6fba978de04d0
Romulo Santana <romulomsantana@hotmail.com>
2023-06-15 11:27:41 -0300

    use css function from styled-components

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,10 +1,15 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.article`
   margin-bottom: 24px;
 
-  opacity: ${(props) => props.removed ? 0.3 : 1};
-  color: ${(props) => props.removed ? '#f00' : '#fff'};
+  /* opacity: ${(props) => props.removed ? 0.3 : 1};
+  color: ${(props) => props.removed ? '#f00' : '#fff'}; */
+
+  ${({ removed }) => css`
+    opacity: ${removed ? 0.3 : 1};
+    color: ${removed ? '#f00' : '#fff'};
+  `}
 `;
 
 export const Subtitle = styled.small`
