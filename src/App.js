cabe69091599b78bd051fc2a414b5e75a426946f
Romulo Santana <romulomsantana@hotmail.com>
2023-06-17 16:16:21 -0300

    create toggleTheme feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/global';
@@ -7,10 +7,24 @@ import Layout from './components/Layout';
 import themes from './styles/themes';
 
 function App() {
+  const [theme, setTheme] = useState('dark');
+
+  const currentTheme = useMemo(() => {
+    return themes[theme] || themes.dark;
+  }, [theme]);
+  // useMemo: memorizar valores
+
+  function handleToggleTheme() {
+    setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+  }
+
   return (
-    <ThemeProvider theme={themes.dark}>
+    <ThemeProvider theme={currentTheme}>
       <GlobalStyle />
-      <Layout />
+      <Layout 
+        onToggleTheme={handleToggleTheme}
+        selectedTheme={theme}  
+      />
     </ThemeProvider>
   );
 };
