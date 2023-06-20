cabc427f3d31d4d9023698e0c9e85ea6c5970fc7
Romulo Santana <romulomsantana@hotmail.com>
2023-06-20 19:49:07 -0300

    remove eventListener on unmount

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {  useEffect } from 'react';
 
 import Header from '../Header';
 import PostsList from '../PostsList';
 import Footer from '../Footer';
 
-export default function Layout({ 
-  onToggleTheme, selectedTheme
-}) {
+export default function Layout({ onToggleTheme, selectedTheme}) {
+  useEffect(() => {
+    // é necessario fazer essa funcao pro eventListener pra depois poder
+    // remover ele; e é necessario remover o eventListener pra poupar
+    // memoria e evitar memory leaks
+    function handleScroll() {
+      console.debug('scrolou');
+    }
+
+    document.addEventListener('scroll', handleScroll);
+
+    return () => document.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
       <Header 
