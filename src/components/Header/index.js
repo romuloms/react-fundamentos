97bbe8a0b73a20a9a98f37a219b72de67ec16c81
Romulo Santana <romulomsantana@hotmail.com>
2023-06-23 10:30:43 -0300

    refactor as class component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Container } from './styles';
 
-export default function Header({ onToggleTheme, selectedTheme }) {
-  return (
-    <Container>
-      <h1>JStack's Blog</h1>
-      <button 
-        type="button" 
-        onClick={onToggleTheme}
-      >
-        {selectedTheme === 'dark' ? '🌞' : '🌚'}
-      </button>
-    </Container>
-  );
+export default class Header extends Component {
+  render() {
+    const { onToggleTheme, selectedTheme } = this.props;
+
+    return (
+      <Container>
+        <h1>JStack's Blog</h1>
+        <button 
+          type="button" 
+          onClick={onToggleTheme}
+        >
+          {selectedTheme === 'dark' ? '🌞' : '🌚'}
+        </button>
+      </Container>
+    );
+  }
 }
