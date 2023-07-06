8307b3fcf5d226ad9bb15290bbc2055acca95ff0
Romulo Santana <romulomsantana@hotmail.com>
2023-07-05 22:47:12 -0300

    use states with class components

--- src/App.js
+++ src/App.js
@@ -6,46 +6,73 @@ import Layout from './components/Layout';
 
 import themes from './styles/themes';
 
-function App() {
-  const [theme, setTheme] = useState('dark');
-
-  const firstRender = useRef(true);
-
-  const currentTheme = useMemo(() => {
-    return themes[theme] || themes.dark;
-  }, [theme]);
-  // useMemo: memorizar valores
-
-  function handleToggleTheme() {
-    setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+class App extends React.Component {
+  constructor(props) {
+    // super é o construtor da classe pai no javascript
+    super(props);   // boas praticas em react (repassar as props para o super)
+    
+    this.state = {
+      theme: 'dark',
+    };
   }
 
-  useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false;
-      return;
-    }
+  render() {
+    const { theme } = this.state
 
-    console.debug({ theme });
-  }, [theme]);
+    function handleToggleTheme() {}
 
-  // useLayoutEffect(() => {
-  //   for (let i = 0; i < 1500; i++) {
-  //     console.debug(i);
-  //   }
+    return (
+      <ThemeProvider theme={themes[theme] || themes.dark}>
+        <GlobalStyle />
+        <Layout 
+          onToggleTheme={handleToggleTheme}
+          selectedTheme={theme}  
+        />
+      </ThemeProvider>
+    );
+  }
+}
 
-  //   // executado de forma sincrona, trava a aplicacao
-  // });
+// function App() {
+//   const [theme, setTheme] = useState('dark');
 
-  return (
-    <ThemeProvider theme={currentTheme}>
-      <GlobalStyle />
-      <Layout 
-        onToggleTheme={handleToggleTheme}
-        selectedTheme={theme}  
-      />
-    </ThemeProvider>
-  );
-};
+//   const firstRender = useRef(true);
+
+//   const currentTheme = useMemo(() => {
+//     return themes[theme] || themes.dark;
+//   }, [theme]);
+//   // useMemo: memorizar valores
+
+//   function handleToggleTheme() {
+//     setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+//   }
+
+//   useEffect(() => {
+//     if (firstRender.current) {
+//       firstRender.current = false;
+//       return;
+//     }
+
+//     console.debug({ theme });
+//   }, [theme]);
+
+//   // useLayoutEffect(() => {
+//   //   for (let i = 0; i < 1500; i++) {
+//   //     console.debug(i);
+//   //   }
+
+//   //   // executado de forma sincrona, trava a aplicacao
+//   // });
+
+//   return (
+//     <ThemeProvider theme={currentTheme}>
+//       <GlobalStyle />
+//       <Layout 
+//         onToggleTheme={handleToggleTheme}
+//         selectedTheme={theme}  
+//       />
+//     </ThemeProvider>
+//   );
+// };
 
 export default App;
